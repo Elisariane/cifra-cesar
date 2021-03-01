d8858d3d0b364c41a4234bb82603a0ea79227530
Elisariane <elisarianeBS@gmail.com>
2021-03-01 19:44:44 -0300

    Refatoração no código

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,34 +1,15 @@
-msgCriptografada = ''
-msgDescriptografada = ''
-alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
 def encodeMensagem(valorChave, msg):
-    msgEncoded = []
-
+    m = ''
     for letra in msg:
-        if (letra not in alfabeto):
-            msgEncoded.append(letra)
-        else:
-            posicao = (alfabeto.index(letra) + valorChave) % 52
-            msgEncoded.append(alfabeto[posicao])
-    msgCriptografada = ''.join(msgEncoded)
-    return msgCriptografada
+        m += chr(ord(letra) + valorChave)
+    return m
 
 
 def decodeMensagem(valorChave, msg):
-    msgDecoded = []
+    m = ''
     for letra in msg:
-        if (letra not in alfabeto):
-            msgDecoded.append(letra)
-        else:
-            posicao = (alfabeto.index(letra) - valorChave) % 52
-            msgDecoded.append(alfabeto[posicao])
-    msgDescriptografada = ''.join(msgDecoded)
-    return msgDescriptografada
+        m += chr(ord(letra) - valorChave)
+    return m
 
 
 print('==================== CIFRA DE CÉSAR ===============')
@@ -37,7 +18,8 @@ msg = str(input("Qual a mensagem que será criptografada? \n > "))
 
 valorChave = int(input("Qual o valor da chave? \n > "))
 
-print('Mensagem Criptografada: ', encodeMensagem(valorChave, msg))
+encoded = encodeMensagem(valorChave, msg)
 
-print('Mensagem descriptografada: ',
-      decodeMensagem(valorChave, encodeMensagem(valorChave, msg)))
+print('Mensagem Criptografada: ', encoded)
+print('Mensagem Descriptografada: ',
+      decodeMensagem(valorChave, encoded))
